707afd9a92704e5a04865d6e05e31a4f86e71286
Tahir Anil Oghan <t.anil.oghan@gmail.com>
2025-07-23 22:18:12 +0300

    Initial Commit

---
+++ DeviceUtility/DeviceUtilityApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeviceUtilityApp.swift
+//  DeviceUtility
+//
+//  Created by Tahir Anil Oghan on 23.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct DeviceUtilityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DeviceUtilityTests/DeviceUtilityTests.swift
@@ -0,0 +1,17 @@
+//
+//  DeviceUtilityTests.swift
+//  DeviceUtilityTests
+//
+//  Created by Tahir Anil Oghan on 23.07.2025.
+//
+
+import Testing
+@testable import DeviceUtility
+
+struct DeviceUtilityTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
